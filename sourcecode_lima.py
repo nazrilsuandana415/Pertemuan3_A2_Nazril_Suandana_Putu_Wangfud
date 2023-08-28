340d04c297dade82dd916056b7f8fecea0218019
nazrilsuandana415 <142768666+nazrilsuandana415@users.noreply.github.com>
2023-08-28 10:41:15 +0700

    Add files via upload

---
+++ sourcecode_lima.py
@@ -0,0 +1,13 @@
+blog = 'Klinik' + 'Python'
+print(blog)
+newblog = blog*5
+newblog
+blog *= 4
+print(blog)
+string = 'Klinik' 'Python'
+string
+buah = 'Nanas'
+print(buah[0])
+print(buah[0:2])
+print(buah[0:4])
+print(buah[0:5])
